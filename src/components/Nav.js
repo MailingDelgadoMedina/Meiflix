e1b30027eeb003c03ba36d0d0d8cf223f9dfbec6
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-05-17 15:20:36 -0400

    added images and styling the nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,15 +3,20 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>
+    <div className='nav'>
+      <div className='nav-contents flex justify-between'>
 
-        <img src="https://res.cloudinary.com/programandoconmei/image/upload/v1652774354/meiflix/MEIFLIX_qjjlrc.svg"
+      <img src="https://res.cloudinary.com/programandoconmei/image/upload/v1652775622/meiflix/MEIFLIX_500_200_px_qjkjmb.svg"
               alt="Logo"
                 />
         <img
+        className='w-24 h-24'
         src='https://res.cloudinary.com/programandoconmei/image/upload/v1652774834/meiflix/avatar_u3wclb.jpg'
         alt='avatar'
         />
+
+
+      </div>
 
 
             </div>
