4af1923bef354f134954f9d86d4b4725d62c0db5
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-05-17 04:11:41 -0400

    added images to the nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,18 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+
+        <img src="https://res.cloudinary.com/programandoconmei/image/upload/v1652774354/meiflix/MEIFLIX_qjjlrc.svg"
+              alt="Logo"
+                />
+        <img
+        src='https://res.cloudinary.com/programandoconmei/image/upload/v1652774834/meiflix/avatar_u3wclb.jpg'
+        alt='avatar'
+        />
+
+
+            </div>
   )
 }
 
